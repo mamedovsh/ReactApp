71232dc9fb7f36d0a8d23baca65e89a141793979
mamedovsh <shagin.mamedoff@yandex.ru>
2024-09-07 11:14:41 +0700

    Решение задачи к семинару №1

---
+++ src/Components/Message/Message.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './Message.css';
+
+const Message = ({text}) =>{
+    return(
+        <div className="message">
+            {text}
+        </div>
+    );
+}
+
+export default Message;
